be69676ceccc33392f6cf4c20738ef1f1ba06e50
Wajd <wajdalhadlaq@gmail.com>
2024-09-30 23:06:08 +0300

    submit

--- src/app/job-application-form/job-application-form.component.ts
+++ src/app/job-application-form/job-application-form.component.ts
@@ -1,39 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-job-application-form',
   templateUrl: './job-application-form.component.html',
-  styleUrls: ['./job-application-form.component.css'],
-
+  styleUrls: ['./job-application-form.component.css']
 })
-export class JobApplicationFormComponent implements OnInit{
- 
-
-public jobApplication: FormGroup;
-
-  constructor() {  }
-
+export class JobApplicationFormComponent implements OnInit {
   
+  public jobApplication: FormGroup;
 
-  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initForm();
   }
-  onSubmit(){
-    console.log(this.jobApplication.value)
-  
+
+  private initForm(): void {
+    // Initialize form with FormBuilder
+    this.jobApplication = this.fb.group({
+      personalInfo: this.fb.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', Validators.required]
+      }),
+      experience: this.fb.group({
+        yearsExperience: ['', Validators.required],
+        jobTitle: ['', Validators.required]
+      })
+    });
   }
 
-private initForm(): void{
-  this.jobApplication = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    phone: new FormControl(null, Validators.required),
-  });
-}
-
-  
+  // Form submission method
+  onSubmit(): void {
+    if (this.jobApplication.valid) {
+      console.log('Form Submitted!', this.jobApplication.value);
+    } else {
+      console.log('Form is invalid.');
+    }
+  }
 }
